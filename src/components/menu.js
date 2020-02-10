38f437979ecf95b51e3884e3d63bd0e4dde67eb9
HarjotSingh8 <harjotsinghsandhu800@gmail.com>
2020-02-10 14:45:06 +0530

    updates

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-
+import "./menu.css";
 class Menu extends Component {
   state = { menu: [] };
   componentDidMount() {
-    fetch("https://canteen-ordering-3d30c.firebaseio.com/public/Tea%20Q.json")
+    fetch(
+      "https://canteen-ordering-3d30c.firebaseio.com/public/LaPinoz/veg.json"
+    )
       .then(res => res.json())
       .then(res => this.setState({ menu: res }));
   }
@@ -18,11 +20,36 @@ class Menu extends Component {
         style={{ width: "100%" }}
       >
         <div className="row">
-          {Object.keys(this.state.menu).map(key => {
+          {Object.keys(this.state.menu).map(menuItem => {
             return (
-              <div className="card col-4">
-                <div className="card-body"> {this.state.menu[key].name}</div>
-                <div> {this.state.menu[key].price}</div>
+              <div key={menuItem} className=" col-xs-12 col-md-6 col-lg-4">
+                <div
+                  className="card bg-danger m-1 embed-responsive embed-responsive-16by9"
+                  style={{ overflow: "hidden" }}
+                >
+                  <img
+                    className="card-img embed-responsive-item"
+                    src={this.state.menu[menuItem].image}
+                    style={{ objectFit: "cover" }}
+                  ></img>
+                  <div className="card-img-overlay mx-1">
+                    {/*<div className="row justify-content-center">*/}
+                    <h5 className="card-title d-inline-flex text-white">
+                      {menuItem}
+                    </h5>
+                    {/*</div>*/}
+                    <button
+                      className="btn customButton ml-auto"
+                      style={{
+                        position: "absolute",
+                        right: "1rem",
+                        bottom: "1rem"
+                      }}
+                    >
+                      Add To Cart
+                    </button>
+                  </div>
+                </div>
               </div>
             );
           })}
