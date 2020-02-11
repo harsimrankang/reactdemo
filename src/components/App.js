91e51189e68a4317a657d368b01bfbe5bc695fbb
harsimrankang <kang.harsimran1999@gmail.com>
2020-02-11 08:35:06 +0530

    rough changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Navbar from "./navbar";
 import Menu from "./menu";
-
+import Burgrill from "./Burgrillmenu";
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
         <Menu />
+      </div>
+      <div>
+        <Burgrill />
       </div>
     );
   }
